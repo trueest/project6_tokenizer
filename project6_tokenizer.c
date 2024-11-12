082adf3c218b15b4e3403b9af01fcbe685853576
Hunter Hawkins <95057447+trueest@users.noreply.github.com>
2024-11-12 18:54:59 -0500

    Update project6_tokenizer.c

--- project6_tokenizer.c
+++ project6_tokenizer.c
@@ -2,7 +2,12 @@
 * Name: Hunter Hawkins - U13938229
 * Email: hunterhawkins@usf.edu 
 * Assignment: Project 6: Task 1
-* This C program will
+* This C program will act as a tokenizer to convert an input of words into tokens. 
+* It will achieve this by receiving a text file as input and a text file for output in the command-line.
+* Then, it will read the input file, guaranteed to have at most 10,000 characters.
+* Once read, it will use C library functions to tokenize the input, create a unique array of words from the input,
+* sort the input in alphabetical order, and finally create corresponding numbers, or token values, for each unique word.
+* Finally, it will output all of this information to a write file per requirements.
 */
 
 // ADDING LIBRARIES
@@ -13,8 +18,8 @@
 // Initializing our max character count per requirements.
 #define MAX_LEN 10000
 
-// Comparison function for qsort.
-int compare_strings(const void *a, const void *b) {
+// Comparison function to be used for qsort. Because qsort takes void pointers as arguments, they will be casted to char.
+int compare_words(const void *a, const void *b) {
     return strcmp(*(const char **)a, *(const char **)b);
 }
 
@@ -42,85 +47,104 @@ int main (int argc, char *argv[]) {
     * our main text string array, then our delimiters for the strtok function and 
     * tokenized array for strtok.
     */
-    char buffer[MAX_LEN + 1] = ""; // our input buffer
-    char text[MAX_LEN + 1] = ""; // our array of text, concatenated by strcat and read line by line by fgets
-    char *delimiters = ". \n";
-    char *token;
+    char buffer[MAX_LEN + 1] = ""; // Our input buffer
+    char text[MAX_LEN + 1] = ""; // Our array of text, concatenated by strcat and read line by line by fgets.
+    char *delimiters = ". \n"; // Our delimiters that will be used for the first call of strtok.
+    char *token; // Our tokenized text array.
 
     // Read the file line by line with fgets and adding to our buffer and text arrays.
     while (!feof(rFile) && !ferror (rFile)) {
         while(fgets(buffer, MAX_LEN, rFile)) {
+            // Adding our text from our file/buffer to our text array.
             strcat(text, buffer);
         }
     } 
 
+    // Initializing our array that will hold unique words.
     char *words[MAX_LEN + 1];
+    // Initializing a word_count variable to hold the number of words and be referenced when creating token values for words.
     int word_count = 0;
 
-    //next we will tokenize our text array in *tok
+    // Tokenizing our text with the declared delimiters.
     token = strtok(text, delimiters);
+    // While loop to continue tokenizing until a NULL pointer is returned.
     while(token != NULL) {
+        // Unique flag for the current word.
         int unique = 1;
+        // If the word is already in our words array, then it is not unique and will not be added.
         for (int i = 0; i < word_count; i++) {
             if (strcmp(words[i], token) == 0) {
                 unique = 0;
                 break;
             }
         }
-
+        // If the word is unique, it will be added to the words array.
         if (unique) {
+
+            // Use malloc to allocate memory for the unique word + null character due to the use of strtok.
             words[word_count] = malloc(strlen(token) + 1);
+
+            // Copy the unique word from token into our words array at the word_count index.
             strcpy(words[word_count], token);
+
+            // Increment word count.
             word_count++;
         }
         token = strtok(NULL, delimiters);
     }
 
-    // Using the qsort function with our compare_strings function to alphabetically sort our unique words.
-    qsort(words, word_count, sizeof(char *), compare_strings);
+    // Using the library qsort function with our compare_words function to alphabetically sort our unique words.
+    qsort(words, word_count, sizeof(char *), compare_words);
 
-    // Print or write the sorted words
+    // Write each word to the output file.
     fprintf(wFile, "%d\n", word_count);
     for (int i = 0; i < word_count; i++) {
-        fprintf(wFile, "%s\n", words[i]);  // Write each word to output file
+        fprintf(wFile, "%s\n", words[i]);
     }
 
-    // Create a map of word to index
+    // In order to create an index, or integer value for each word, a word index array will be used.
     int word_indices[MAX_LEN];
+
     for (int i = 0; i < word_count; i++) {
-        word_indices[i] = i + 1;  // Assign numbers starting from 1
+
+        // Since our token values will begin from 1, begin assigning values in our word_indices array at 1.
+        word_indices[i] = i + 1;
     }
 
-    // Re-read text and tokenize by sentences
+    // Reset the file stream position to the beginning of the file to begin reading the input again.
     rewind(rFile);
+
+    // Reset our text array.
     text[0] = '\0';
+
+    // Read the file line by line with fgets and adding to our buffer and text arrays.
     while (fgets(buffer, MAX_LEN, rFile)) {
         strcat(text, buffer);
     }
 
-    // Tokenize sentences by periods
-    char *sentences_token = strtok(text, ".");
-    while (sentences_token != NULL) {
-        // Create a copy of the sentence for word tokenization
-        char sentence_copy[MAX_LEN + 1];
-        strcpy(sentence_copy, sentence_token);
+    // Process each sentence by finding periods
+    char *sentence_start = text;
+    char *period_position;
+    while ((period_position = strchr(sentence_start, '.')) != NULL) {
+        // Temporarily end the sentence at the period
+        *period_position = '\0';
 
-        // Tokenize words within the sentence copy
-        token = strtok(sentence_copy, " \n");
-        while (token != NULL) {
+        // Tokenize words within the sentence
+        char *word_token = strtok(sentence_start, " \n");
+        while (word_token != NULL) {
             // Find index of the word in the sorted list
             for (int i = 0; i < word_count; i++) {
-                if (strcmp(words[i], token) == 0) {
+                if (strcmp(words[i], word_token) == 0) {
                     fprintf(wFile, "%d ", word_indices[i]);
                     break;
                 }
             }
-            token = strtok(NULL, " \n");
+            word_token = strtok(NULL, " \n");
         }
         fprintf(wFile, "\n");  // Newline after each sentence's word numbers
 
         // Move to the next sentence
-        sentence_token = strtok(NULL, ".");
+        sentence_start = period_position + 1;  // Start after the period
     }
 
     // Free memory
